6a58a3f1d7f74dded57d00825218a4e8385a041a
priteshparmar9 <74969871+priteshparmar9@users.noreply.github.com>
2023-05-23 18:44:48 +0530

    Frontend for NavBar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -11,154 +11,376 @@ import {
   MenuList,
   MenuButton,
   MenuItem,
+  Drawer,
+  DrawerBody,
+  Stack,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import logo from "../static/images/logo.png";
 
 const Navbar = (props) => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   let loggedin = props.loggedin;
   let setLoggedIn = props.setLoggedIn;
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
   return (
-    <Box mb="7">
-      <Flex
-        mr="4px"
-        flexDir="row"
-        minW="100%"
-        justifyContent="center"
-        bgColor="rgb(60, 72, 107)"
-        top="0"
-        pos="fixed"
-        zIndex={2}
-      >
+    <Box mb="7" bgColor="red">
+      {!isMobile ? (
         <Flex
+          mr="4px"
           flexDir="row"
-          mt="2px"
-          mb="2px"
-          w="85%"
-          justifyContent="space-between"
-          pos="relative"
-          transition="all 1s"
-          zIndex="2"
-          p="5px"
+          minW="100%"
+          justifyContent="center"
+          bgColor="rgb(60, 72, 107)"
+          top="0"
+          pos="fixed"
+          zIndex={2}
         >
-          <Link to="/">
-            <Image src={logo} h="5vmin" />
-          </Link>
-          <InputGroup ml="130px" maxW="50vmin">
-            <Input
-              placeholder="Search anything..."
-              bgColor="white"
-              variant="flushed"
-              borderRadius="lg"
-              pl="5px"
-              value={query}
-              onChange={(e) => {
-                setQuery(e.target.value);
-              }}
-            />
+          <Flex
+            flexDir="row"
+            mt="2px"
+            mb="2px"
+            w="85%"
+            justifyContent="space-between"
+            pos="relative"
+            transition="all 1s"
+            zIndex="2"
+            p="5px"
+          >
+            <Link to="/">
+              <Image src={logo} h="5vmin" />
+            </Link>
+            <InputGroup ml="130px" maxW="50vmin">
+              <Input
+                placeholder="Search anything..."
+                bgColor="white"
+                variant="flushed"
+                borderRadius="lg"
+                pl="5px"
+                value={query}
+                onChange={(e) => {
+                  setQuery(e.target.value);
+                }}
+              />
 
-            <InputRightAddon
-              cursor="pointer"
-              onClick={() => {
-                if (query) {
-                  navigate(`/search/${query}`);
-                }
-              }}
-            >
-              <FiSearch />
-            </InputRightAddon>
-          </InputGroup>
-          {!loggedin.loggedin ? (
-            <HStack>
-              <Link to="/login">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                >
-                  Login
-                </Button>
-              </Link>
-              <Link to="/signup">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                >
-                  Signup
-                </Button>
-              </Link>
-              <Link to="/login">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                >
-                  Login
-                </Button>
-              </Link>
-              <Link to="/login">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                >
-                  Login
-                </Button>
-              </Link>
-            </HStack>
-          ) : (
-            <HStack>
-              <Menu>
-                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                  My Account
-                </MenuButton>
-                <MenuList>
-                  <MenuItem
+              <InputRightAddon
+                cursor="pointer"
+                onClick={() => {
+                  if (query) {
+                    navigate(`/search/${query}`);
+                  }
+                }}
+              >
+                <FiSearch />
+              </InputRightAddon>
+            </InputGroup>
+            {!loggedin.loggedin ? (
+              <HStack>
+                <Link to="/login">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
+                  >
+                    Login
+                  </Button>
+                </Link>
+                <Link to="/signup">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
+                  >
+                    Signup
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
+                  >
+                    Login
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
+                  >
+                    Login
+                  </Button>
+                </Link>
+              </HStack>
+            ) : (
+              <HStack>
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    My Account
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem
+                      onClick={() => {
+                        navigate("/currentWinnings");
+                      }}
+                    >
+                      Current Winnings
+                    </MenuItem>
+                    <MenuItem>Remaining Payments</MenuItem>
+                    <MenuItem>History</MenuItem>
+                    <MenuItem>My Orders</MenuItem>
+                    <MenuItem>Update Profile</MenuItem>
+                  </MenuList>
+                </Menu>
+
+                <Link to="/expired">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
+                  >
+                    Ended
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button
+                    bgColor="transparent"
+                    color="rgb(240, 240, 240)"
+                    _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
                     onClick={() => {
-                      navigate("/currentWinnings");
+                      window.localStorage.clear();
+                      setLoggedIn({ ...loggedin, loggedin: false });
                     }}
                   >
-                    Current Winnings
-                  </MenuItem>
-                  <MenuItem>Remaining Payments</MenuItem>
-                  <MenuItem>History</MenuItem>
-                  <MenuItem>My Orders</MenuItem>
-                  <MenuItem>Update Profile</MenuItem>
-                </MenuList>
-              </Menu>
-
-              <Link to="/expired">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                >
-                  Ended
-                </Button>
-              </Link>
-              <Link to="/login">
-                <Button
-                  bgColor="transparent"
-                  color="rgb(240, 240, 240)"
-                  _hover={{ color: "black", bgColor: "rgb(240, 240, 240)" }}
-                  onClick={() => {
-                    window.localStorage.clear();
-                    setLoggedIn({ ...loggedin, loggedin: false });
-                  }}
-                >
-                  Logout
-                </Button>
-              </Link>
-            </HStack>
-          )}
+                    Logout
+                  </Button>
+                </Link>
+              </HStack>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
+      ) : (
+        <Flex
+          mr="4px"
+          flexDir="row"
+          minW="100%"
+          justifyContent="center"
+          bgColor="rgb(60, 72, 107)"
+          top="0"
+          pos="fixed"
+          zIndex={1}
+        >
+          <Flex
+            flexDir="row"
+            mt="2px"
+            mb="2px"
+            w="85%"
+            justifyContent="space-between"
+            pos="relative"
+            transition="all 1s"
+            zIndex="2"
+            p="5px"
+          >
+            <Link to="/">
+              <Image src={logo} h="10vmin" />
+            </Link>
+            <HamburgerIcon
+              ref={btnRef}
+              color="white"
+              onClick={onOpen}
+              mt="10px"
+            />
+
+            <Drawer
+              isOpen={isOpen}
+              placement="right"
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>E-Auction</DrawerHeader>
+
+                <DrawerBody mt="20px">
+                  <Flex flexDir="column">
+                    <InputGroup minW="50vmin">
+                      <Input
+                        placeholder="Search anything..."
+                        bgColor="white"
+                        variant="flushed"
+                        borderRadius="lg"
+                        pl="5px"
+                        value={query}
+                        onChange={(e) => {
+                          setQuery(e.target.value);
+                        }}
+                      />
+
+                      <InputRightAddon
+                        cursor="pointer"
+                        onClick={() => {
+                          if (query) {
+                            navigate(`/search/${query}`);
+                          }
+                        }}
+                      >
+                        <FiSearch />
+                      </InputRightAddon>
+                    </InputGroup>
+                    {!loggedin.loggedin ? (
+                      <Flex mt="20px" flexDir="column" display="flex">
+                        <Link to="/login">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Login
+                          </Button>
+                        </Link>
+                        <Link to="/signup">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Signup
+                          </Button>
+                        </Link>
+                        <Link to="/login">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Login
+                          </Button>
+                        </Link>
+                        <Link to="/login">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Login
+                          </Button>
+                        </Link>
+                      </Flex>
+                    ) : (
+                      <Flex flexDir="column" mt="20px">
+                        <Link to="/currentWinnings">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Current Winnings
+                          </Button>
+                        </Link>
+                        <Link to="/currentWinnings">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Remaining Payments
+                          </Button>
+                        </Link>
+                        <Link to="/currentWinnings">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            History
+                          </Button>
+                        </Link>
+                        <Link to="/currentWinnings">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            My Orders
+                          </Button>
+                        </Link>
+                        <Link to="/currentWinnings">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Update Profile
+                          </Button>
+                        </Link>
+
+                        <Link to="/expired">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                          >
+                            Ended
+                          </Button>
+                        </Link>
+                        <Link to="/login">
+                          <Button
+                            bgColor="transparent"
+                            _hover={{
+                              color: "black",
+                              bgColor: "rgb(240, 240, 240)",
+                            }}
+                            onClick={() => {
+                              window.localStorage.clear();
+                              setLoggedIn({ ...loggedin, loggedin: false });
+                            }}
+                          >
+                            Logout
+                          </Button>
+                        </Link>
+                      </Flex>
+                    )}
+                  </Flex>
+                </DrawerBody>
+              </DrawerContent>
+            </Drawer>
+          </Flex>
+        </Flex>
+      )}
     </Box>
   );
 };
